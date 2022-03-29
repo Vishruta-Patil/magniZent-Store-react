775cd00aeb093da91d18a1098578f5a7e63764ee
Vishruta-Patil <vishrutapatil2106@gmail.com>
2022-03-30 00:59:32 +0530

    feat: cart

--- src/pages/Wishlist/index.js
+++ src/pages/Wishlist/index.js
@@ -1,75 +1,50 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useWishList } from "../../context/wishlistContext";
-import { WISHLIST_LOADER, WISHLIST_DATA } from "../../reducer/wishlist/wishlistConstants";
 import ProductCard from "../../components/common/ProductCard";
-import EmptyWishList from "./emptyWishList";
 import Loader from "../../components/common/Loader";
+import EmptyBag from "../../components/common/EmptyBag";
+import { getWishlistItems, deleteWishListHandler } from "../../utils/handler";
 
 export const Wishlist = () => {
   const { state, dispatch } = useWishList();
-  const encodedtoken = localStorage.getItem("token");
-
-  useEffect(
-    () =>
-      (async () => {
-        {console.log("wishlist1: ", state.wishListLoader)}
-        dispatch({type: WISHLIST_LOADER})
-        {console.log("wishlist2: ", state.wishListLoader)}
-        try {
-          const response = await axios.get("/api/user/wishlist", {
-            headers: {
-              authorization: encodedtoken,
-            },
-          });
-
-          dispatch({type: WISHLIST_LOADER})
-          {console.log("wishlist3: ", state.wishListLoader)}
-          dispatch({ type: WISHLIST_DATA, payload: response.data.wishlist });
-        } catch (err) {
-          dispatch({type: WISHLIST_LOADER})
-          console.log(err);
-        }
-      })(),
-    []
-  );
-
-  const deleteWishListHandler = async ({ item }) => {
-    try {
-      const config = {
-        headers: {
-          authorization: localStorage.getItem("token"),
-        },
-      };
-      
-      const response = await axios.delete(`/api/user/wishlist/${item._id}`, config);
-      console.log("wish res: " + response.data.wishlist)
-      dispatch({ type: WISHLIST_DATA, payload: response.data.wishlist });
-
-    } catch (err) {
-      console.log("wish err:- " + err);
-    }
-  };
+  useEffect(() => getWishlistItems(dispatch), []);
 
   return (
     <div>
-      {state.wishListLoader ? <Loader /> :
-    <div class="wishlist-container">
-      <h2 class="header-wishlist">My Wishlist</h2>
+      {state.wishListLoader ? (
+        <Loader />
+      ) : (
+        <div class="wishlist-container">
+          <h2 class="header-wishlist">
+            My Wishlist ({state.wishListData.length})
+          </h2>
 
-      <div>
-        {state.wishListData.length === 0 ? (
-          <EmptyWishList />
-        ) : (
-          <div class="wishlist-unit">
-            {state.wishListData.map((data,index)=> (
-             <ProductCard item={data} key={index} clickHandler={deleteWishListHandler} from={true}/>
-            ))}
+          <div>
+            {state.wishListData.length === 0 ? (
+              <EmptyBag
+                name={"Wishlist"}
+                img={
+                  "https://res.cloudinary.com/debanftke/image/upload/v1648206283/wishlist_empty_pcbldx.jpg"
+                }
+              />
+            ) : (
+              <div class="wishlist-unit">
+                {state.wishListData.map((data, index) => (
+                  <ProductCard
+                    item={data}
+                    key={index}
+                    clickHandler={({ item }) =>
+                      deleteWishListHandler(dispatch, { item })
+                    } // doubt
+                    from={true}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
-}
+        </div>
+      )}
     </div>
   );
 };
--- src/utils/handler.js
+++ src/utils/handler.js
@@ -1,16 +1,140 @@
-export const addToWishListHandler = async (item) => {
-    try {
-      const config = {
-        headers: {
-          authorization: localStorage.getItem("token"),
-        },
-      };
-      const data = {
-        product: item,
-      };
-      await axios.post("/api/user/wishlist", data, config);
+import axios from "axios";
+import {
+  WISHLIST_LOADER,
+  CART_DATA,
+  WISHLIST_DATA,
+  CART_INCREMENT,
+  CART_DECREMENT,
+  TOTAL_PRICE
+} from "../reducer/wishlist/wishlistConstants";
+import {
+  PRODUCT_LIST_DATA,
+  LOADING_SPINNER,
+} from "../reducer/productList/productConstants";
+import { USER_LOADING, LOGIN_STATUS } from "../reducer/user/userConstants";
 
-    } catch (err) {
-      console.log("err:- " + err);
-    }
-  };
+const encodedtoken = localStorage.getItem("token");
+const config = {
+  headers: {
+    authorization: encodedtoken,
+  },
+};
+
+
+// Auth
+export const login = async (email, password, dispatch, navigate) => {
+  try {
+    const response = await axios.post("/api/auth/login", {
+      email,
+      password,
+    });
+    localStorage.setItem("token", response.data.encodedToken);
+    navigate("/");
+    dispatch({ type: USER_LOADING });
+    dispatch({type: LOGIN_STATUS})
+    setTimeout(() => dispatch({ type: USER_LOADING }), 1000);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
+
+
+// Product List
+export const getProductList = async (dispatch) => {
+  dispatch({ type: LOADING_SPINNER });
+  try {
+    const response = await axios.get("/api/products");
+    dispatch({ type: LOADING_SPINNER });
+    dispatch({ type: PRODUCT_LIST_DATA, payload: response.data.products });
+  } catch (err) {
+    dispatch({ type: LOADING_SPINNER });
+    console.log("error: ", err);
+  }
+};
+
+
+// Wishlist
+export const getWishlistItems = async (dispatch) => {
+  dispatch({ type: WISHLIST_LOADER });
+  try {
+    const response = await axios.get("/api/user/wishlist", config);
+    dispatch({ type: WISHLIST_LOADER });
+    dispatch({ type: WISHLIST_DATA, payload: response.data.wishlist });
+  } catch (err) {
+    dispatch({ type: WISHLIST_LOADER });
+    console.log("error: ", err);
+  }
+};
+
+export const addToWishListHandler = async ({ item }) => {
+  try {
+    const data = {
+      product: item,
+    };
+    const response = await axios.post("/api/user/wishlist", data, config);
+  } catch (err) {
+    console.log("error:- " + err);
+  }
+};
+
+export const deleteWishListHandler = async (dispatch, { item }) => {
+  try {
+    const response = await axios.delete(
+      `/api/user/wishlist/${item._id}`,
+      config
+    )
+    dispatch({ type: WISHLIST_DATA, payload: response.data.wishlist });
+  } catch (err) {
+    console.log("error:- " + err);
+  }
+};
+
+
+// Cart
+export const getCartItems = async (dispatch) => {
+  dispatch({ type: WISHLIST_LOADER });
+  try {
+    const response = await axios.get("/api/user/cart", config);
+    dispatch({ type: WISHLIST_LOADER });
+    dispatch({ type: CART_DATA, payload: response.data.cart });
+  } catch (err) {
+    dispatch({ type: WISHLIST_LOADER });
+    console.log("error: ", err);
+  }
+};
+
+export const addToCart = async (item,dispatch) => {
+  try {
+  const data = {
+    product: item
+  }
+    const response = await axios.post("/api/user/cart", data, config)
+    dispatch({ type: CART_DATA, payload: response?.data?.cart });
+    dispatch({type: TOTAL_PRICE, payload: item.product_price, quantity:item.qty})
+  } catch(err) {
+
+    console.log("error: " + err)
+  }
+}
+
+export const deleteCartHandler = async (item, dispatch) => {
+  try {
+    const response = await axios.delete(`/api/user/cart/${item?._id}`, config)
+    dispatch({ type: CART_DATA, payload: response?.data?.cart })
+  } catch(err) {
+    console.log("error: " + err)
+  }
+}
+
+export const cartQuantityHandler = async(item, actionType, dispatch) => {
+  try {
+    const {data} = await axios.post(`/api/user/cart/${item?._id}`, { action: { type: actionType }}, config)
+    const {cart} = data
+    console.log(cart)
+    dispatch(actionType === "increment" ? {type: CART_INCREMENT, payload: cart} :  {type: CART_DECREMENT, payload: cart})
+    dispatch({type: "CART_QUANTITY", payload:cart.qty})
+  } catch(err) {
+    console.log("error: " + err)
+  }
+}
+
