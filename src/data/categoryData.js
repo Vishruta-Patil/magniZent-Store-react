74ed1dfab3586cba3402bed2e6fa3e0e9caad7f6
Vishruta-Patil <vishrutapatil2106@gmail.com>
2022-04-05 18:51:08 +0530

    feat: category feature in home page

--- src/data/categoryData.js
+++ src/data/categoryData.js
@@ -9,13 +9,13 @@ const categoryData = [
         img_name: "Jewellery",
         img_url: "https://res.cloudinary.com/debanftke/image/upload/v1647432178/j3_b59awd.png"
     },
+    // {
+    //     id: 3,
+    //     img_name: "Electronics",
+    //     img_url: "https://res.cloudinary.com/debanftke/image/upload/v1647667332/surface-pro_qctzmg.jpg"
+    // },
     {
         id: 3,
-        img_name: "Electronics",
-        img_url: "https://res.cloudinary.com/debanftke/image/upload/v1647667332/surface-pro_qctzmg.jpg"
-    },
-    {
-        id: 4,
         img_name: "Home",
         img_url: "https://res.cloudinary.com/debanftke/image/upload/v1647431982/a13_b6u1wb.jpg"
     }
